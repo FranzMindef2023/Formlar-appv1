b7d4ff4952f135b6ecd8ef0272461a1c417c61e2
nukkua <marco.salazar.2014.com@gmail.com>
2024-12-12 11:10:49 -0400

    first commit

---
+++ routes/api.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\RolesController;
+use App\Http\Controllers\Api\AuthController;
+
+
+use App\Http\Controllers\Api\FuerzasController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+// Route::apiResource('/usuarios', UserController::class);
+// Route::apiResource('/rolsUser', RolesController::class);
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('me', [AuthController::class, 'me']);
+});
+
+Route::post('roldeusuario', [UserController::class, 'asignarRoles']);
+
+Route::apiResource('usuarios', UserController::class);
+
+Route::middleware(['jwt.verify'])->group(function () {
+    Route::get('showroluser/{id}',  [UserController::class, 'showroluser']);
+    Route::apiResource('roles', RolesController::class);
+
+
+    Route::apiResource('fuerzas', FuerzasController::class);
+});
