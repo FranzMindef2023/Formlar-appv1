fd1217cb6ca741c26c998baae176d97d5d90e88e
nukkua <marco.salazar.2014.com@gmail.com>
2025-01-20 00:38:35 -0400

    pan

--- app/Http/Controllers/Api/VistaPorcentajeController.php
+++ app/Http/Controllers/Api/VistaPorcentajeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\UnidadesEducativa;
 use App\Models\VistaPorcentaje;
+use Illuminate\Support\Facades\DB;
 
 class VistaPorcentajeController extends Controller
 {
@@ -72,6 +73,40 @@ class VistaPorcentajeController extends Controller
             return response()->json([
                 'success' => false,
                 'error' => $th->getMessage(),
+                'message' => 'Something went wrong!',
+            ], 500);
+        }
+    }
+    public function oficio()
+    {
+        try {
+            DB::raw("
+                WITH ordenados AS (
+                    SELECT
+                        p.id, -- Suponiendo que `id` es la clave primaria de `premilitars`
+                        LPAD(ROW_NUMBER() OVER (ORDER BY cr.id, p.nota_promedio DESC)::TEXT,
+                             LENGTH((SELECT COUNT(*) FROM premilitars WHERE invitado)::TEXT) + 1,
+                             '0') AS nuevo_oficio
+                    FROM premilitars p
+                    JOIN unidades_educativas ue ON p.codigo_unidad_educativa = ue.codigo
+                    JOIN cupos_unidades_educativas cue ON ue.codigo = cue.unidades_educativa_codigo
+                    JOIN centros_reclutamientos cr ON cue.centros_reclutamiento_id = cr.id
+                    WHERE p.invitado
+                )
+                UPDATE premilitars
+                SET oficio = o.nuevo_oficio
+                FROM ordenados o
+                WHERE premilitars.id = o.id;
+            ");
+
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Porcentaje retrieved successfully',
+            ], 200);
+        } catch (\Exception $th) {
+            return response()->json([
+                'success' => false,
                 'message' => 'Something went wrong!',
             ], 500);
         }
--- routes/api.php
+++ routes/api.php
@@ -32,18 +32,6 @@ Route::group([
 Route::post('assign-role', [UserController::class, 'asignarRoles']);
 Route::get('premilitar/estoy-invitado/{ci}', [PremilitarController::class, 'am_i_invited']);
 
-
-
-
-Route::get('porcentajes/{ue_id}/{gestion}', [VistaPorcentajeController::class, 'show_by_ue_gestion']);
-Route::get('porcentajes/{ue_id}', [VistaPorcentajeController::class, 'show_by_ue']);
-
-
-
-
-
-
-
 Route::middleware('auth:api')->group(function () {
     Route::apiResource('fuerzas', FuerzasController::class);
     Route::apiResource('departamentos', DepartamentoController::class);
@@ -59,30 +47,22 @@ Route::middleware('auth:api')->group(function () {
         Route::get('unidades-educativas/{year}', [UnidadesEducativaController::class, 'show_by_gestion']);
     });
 
-
     Route::prefix('cupos')->group(function () {
         Route::apiResource('aperturas', AperturaController::class);
         Route::apiResource('division', CuposDivisionController::class);
         Route::apiResource('centros-reclutamiento', CuposCentrosReclutamientoController::class);
         Route::apiResource('unidades-educativas', CuposUnidadesEducativaController::class);
     });
-
-
-
+    Route::get('porcentajes/oficio', [VistaPorcentajeController::class, 'oficio']);
+    Route::get('porcentajes/{ue_id}/{gestion}', [VistaPorcentajeController::class, 'show_by_ue_gestion']);
+    Route::get('porcentajes/{ue_id}', [VistaPorcentajeController::class, 'show_by_ue']);
 
     Route::get('showroluser/{id}',  [UserController::class, 'showroluser']);
 
     Route::apiResource('usuarios', UserController::class);
     Route::apiResource('roles', RolesController::class);
 
-
-
-
-
     Route::apiResource('premilitar', PremilitarController::class);
     Route::get('premilitar/habilitados-edad', [PremilitarController::class, 'index_habilitados_edad']);
     Route::get('premilitar/invitados', [PremilitarController::class, 'index_invitados']);
-
-
-    // calls to view_porcentajes in the db, how many students male and female by ue
 });
